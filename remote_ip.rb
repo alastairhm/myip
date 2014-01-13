9f9a57722fea58f2c67710d6a9dd6a7a7898e9ac
alastairhm <amontgomery@williamhill.co.uk>
2014-01-13 15:03:51 +0000

    moved twitter keys to seperate file

--- remote_ip.rb
+++ remote_ip.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/ruby
 require 'open-uri'
-require 'twitter'
+require 'twitter' #https://github.com/sferik/twitter for config details
 
+require File.expand_path(File.join(File.dirname(__FILE__), "twitterCode.rb"))
 twitDM = "alastair_hm"
 
 remote_ip = open('http://whatismyip.akamai.com').read
@@ -11,10 +12,10 @@ if (remote_ip != old_ip) then
     File.open('remote_ip.txt', "w") { |file|  file.write(remote_ip)}
 
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ""
-      config.consumer_secret     = ""
-      config.access_token        = ""
-      config.access_token_secret = ""
+        config.consumer_key        = @twitterCodes[0]
+        config.consumer_secret     = @twitterCodes[1]
+        config.access_token        = @twitterCodes[2]
+        config.access_token_secret = @twitterCodes[3]
     end
 
     client.update("D #{twitDM} My IP address is now #{remote_ip}")
